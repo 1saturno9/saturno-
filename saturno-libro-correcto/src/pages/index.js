9003ad8b16703ef45d06c10ffa6a317c8e55752e
1saturno9 <umafuentebuena2@gmail.com>
2025-04-21 01:42:06 -0300

    Add files via upload

---
+++ saturno-libro-correcto/src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#1b1b2f', color: 'white', padding: '2rem', minHeight: '100vh' }}>
+      <h1 style={{ fontSize: '1.8rem', textAlign: 'center' }}>Libro Digital - Estilo Saturno</h1>
+      <p style={{ textAlign: 'center' }}>Subí tu portada, contraportada y contenido PDF.</p>
+    </div>
+  );
+}
